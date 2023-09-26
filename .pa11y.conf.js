0916c49c6c49a91f12506b2cd3f9c1c858ed68de
Joel McConvey <joel.mcconvey@kainos.com>
2023-09-26 11:35:40 +0100

    added secrets

--- .pa11y.conf.js
+++ .pa11y.conf.js
@@ -1,15 +1,9 @@
 var defaults = {
-
     concurrency: 1,
-
     standard: 'WCAG2AA',
-
     timeout: 50000,
-
     chromeLaunchConfig: {
-
         args: ['--no-sandbox'],
-
     }
 }
 
@@ -20,7 +14,6 @@ function myPa11yCiConfiguration() {
     console.error('Env:', process.env.PA11Y_TEST)
 
     for (var idx = 0; idx < urls.length; idx++) {
-
         urls[idx] = urls[idx].replace('${PA11Y_TEST}', process.env.PA11Y_TEST)
     }
 
